3e2bbc21e17ef72b9cededca296bd5e98687cecf
Laukik Deshmukh <deshmukhlaukik52@gmail.com>
2026-07-17 12:46:18 +0530

    Added Day18 Python questions

---
+++ Python/Day18/Calculator/main.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3caba0ee-2536-496f-9722-c6c315954ec0",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
